e265cf7e588748403571d251aee520ea4ca6e991
rb1econ <rb1econ@gmail.com>
2015-04-10 11:35:47 -0600

    jade looping

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
+var Discs = require('../models/post.js')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Discs for Golf' });
+	Discs.find({}, function(err, results){
+		console.log('err (db)', err);
+		console.log('results (db)', results);
+  	res.render('index', { title: 'Discs for Golf', discs: results });
+	})
 });
 
 router.use('/api/', require('./post.js'));
